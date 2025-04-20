da7a5f51ce3a3d25248a275d62a1f80d68c1e6e1
WillWillman <will@willman.codes>
2025-04-19 18:59:36 -0700

    remove history from documentation prompt to reduce tokens

--- src/commands/documentation/documentation.ts
+++ src/commands/documentation/documentation.ts
@@ -6,11 +6,11 @@ export const instructions = [
   'You will be provided with all of the markdown and package.json files in the workspace.',
   'The user may provide additional references to files in chat.',
   'You will answer the user question based on the files.',
-  'Then you will provide a summery of the relevant documentation.',
+  'Then you will provide a summary of the relevant documentation.',
   'Lastly if there are any outdated or conflicting documentation add a section at the end (else leave it out).',
 ];
 
-export const documentation: chathy.Command = (commandContext) => async (request, context, stream, token) => {
+export const documentation: chathy.Command = (commandContext) => async (request, _context, stream, token) => {
 
   const extensionReferences = await chathy.utils.chat.getFileReferences({
     base: commandContext.workspaceRoot,
@@ -21,5 +21,5 @@ export const documentation: chathy.Command = (commandContext) => async (request,
 
   const references = [...request.references, ...extensionReferences];
 
-  return chathy.utils.chat.chatStream(instructions)({ ...request, references }, context, stream, token);
+  return chathy.utils.chat.chatStream(instructions)({ ...request, references }, null, stream, token);
 };
