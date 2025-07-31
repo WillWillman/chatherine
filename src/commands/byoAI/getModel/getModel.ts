726653541f14bae895dbd662534070efea57eafb
William Willman <william.willman@kroger.com>
2025-07-31 09:53:52 -0700

    Sort Model List for byoai

--- src/commands/byoAI/getModel/getModel.ts
+++ src/commands/byoAI/getModel/getModel.ts
@@ -21,7 +21,7 @@ export const getModel = async (commandContext) => {
 
   return aiClient
     .listModels()
-    .then(models => models.sort((a, b) => a.name.localeCompare(b.name)))
+    .then(models => models.sort((a, b) => a.localeCompare(b)))
     .then(models => UserPromptClient.showQuickPick('Select a model', models))
     .then(aiClient.getModel);
 };
